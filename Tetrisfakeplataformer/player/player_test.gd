73543f41405ced4074ffad364a07ff3f33b03535
Raylandson Cesário <www.raylandsoncs@gmail.com>
2023-11-10 14:52:58 -0300

    new player movement

--- Tetrisfakeplataformer/player/player_test.gd
+++ Tetrisfakeplataformer/player/player_test.gd
@@ -1,5 +1,10 @@
 extends RigidBody2D
 
+@export var accel: float = 30.0
+@export var turn_accel: float = 60.0
+@export var max_velocity: float = 200.0
+@export var jump_velocity: float = 300.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,15 +18,19 @@ func _process(delta):
 
 func _physics_process(delta):
 	
-	if sign(linear_velocity.x) == sign(get_direction().x):
-		self.apply_central_impulse(Vector2(get_direction().x * 10, 0))
+	if (sign(linear_velocity.x) == sign(get_direction().x)):
+		linear_velocity.x += get_direction().x * accel
 	elif not(sign(linear_velocity.x) == sign(get_direction().x)):
-		self.apply_central_impulse(Vector2(get_direction().x * 30, 0))
+		linear_velocity.x += get_direction().x * turn_accel
 	
+	if sign(get_direction().x) == 0:
+		linear_velocity.x = max(abs(linear_velocity.x) - 60, 0) * sign(linear_velocity.x)
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		self.apply_central_impulse(Vector2(0, -300))
-		
+		linear_velocity.y = -jump_velocity
 	
+	linear_velocity.x = clamp(linear_velocity.x, -max_velocity, max_velocity)
+	
+	print(linear_velocity.x)
 
 
 func get_direction():
